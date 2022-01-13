0fb43a82c0e80ef31eb084487e75e2f9257650dd
Realmy <43610767+ItsRealmy@users.noreply.github.com>
2022-01-13 14:33:24 +0100

    Create auth router

--- server.js
+++ server.js
@@ -13,6 +13,8 @@ if (process.env.CORS == "true") app.use(require("cors")());
 const mongo = require("mongo-express-req");
 app.use(mongo(process.env.MONGO_URI));
 
+app.use("/auth", require("./routes/auth"));
+
 app.use("*", (req, res) => {
     return res.status(404).send({ status: "NOT_FOUND" });
 });
