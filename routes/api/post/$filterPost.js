a472673da55e51ebea63fa459baf09c7a1821155
Realmy <43610767+ItsRealmy@users.noreply.github.com>
2022-01-19 13:01:43 +0100

    User info on $filterPost.js

--- routes/api/post/$filterPost.js
+++ routes/api/post/$filterPost.js
@@ -1,10 +1,16 @@
+const filterUser = require("../user/$filterUser");
+
 module.exports = async (post, req) => {
+    const users = req.db.collection("users");
+    const creator = await users.find({ _id: post.creator }).toArray();
+    const filteredCreator = await filterUser(creator[0], req);
+
     return {
         id: post._id,
         body: post.body,
-        creator: post.creator,
         likes: post.likes,
         dislikes: post.dislikes,
-        edited: post.edited || false
+        edited: post.edited || false,
+        creator: filteredCreator
     }
 }
